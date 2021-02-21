afb90368b87431428b63deb05cad361ec966dff1
omarayman639 <56203198+omarayman639@users.noreply.github.com>
2021-02-21 20:03:47 +0200

    added brands slider, added filter side bar, added brand products page

--- js/app.js
+++ js/app.js
@@ -4,7 +4,12 @@
 (function ($) {
     // Main Object
     var RESHOP = {};
-
+    // const userAction = async () => {
+    //     const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    //     const myJson = await response.json(); //extract JSON from the http response
+    //     console.log(myJson)
+    //     // do something with myJson
+    //   }
     // Predefined variables
     var
         $filterGridWrapper = $('.filter__grid-wrapper'),
